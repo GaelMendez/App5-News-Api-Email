3c24987c53327cefdfd8185cb550b39d83c559ed
gael <gaeljmendez2090@gmail.com>
2024-11-18 00:01:33 -0600

    Send an email with titles and descriptions

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import requests
+import os
+import functions
+from send_email import send_email
 
-api_key = "bc9de7877a3f455284a043fa0f3a14f9"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "from=2024-10-18&sortBy=publishedAt&apiKey=" \
-      "bc9de7877a3f455284a043fa0f3a14f9"
+api_key = os.getenv("APIKEY")
 
-request = requests.get(url)
-content = request.text
-print(content)
+params = {
+"language": "en"
+}
+
+# Make request
+request = requests.get(functions.get_url(),params)
+
+# Get a dictionary with data
+content = request.json()
+
+#Access the article titles and description
+body = ""
+for article in content["articles"]:
+      body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
+
+
