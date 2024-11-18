291e98102040e84c6632d9c5b2fbfd4cc70e7a77
gael <gaeljmendez2090@gmail.com>
2024-11-18 00:11:05 -0600

    Add links, limit views, add subject, control category

--- main.py
+++ main.py
@@ -9,16 +9,21 @@ params = {
 "language": "en"
 }
 
+topic = "tesla"
+
 # Make request
-request = requests.get(functions.get_url(),params)
+request = requests.get(functions.get_url(topic),params)
 
 # Get a dictionary with data
 content = request.json()
 
 #Access the article titles and description
 body = ""
-for article in content["articles"]:
-      body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
+for article in content["articles"][:20]:
+      body = "Subject: Today's news"\
+             + "\n" +body + article["title"] + "\n"\
+             + str(article["description"]) + "\n"\
+             + article["url"] +2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
